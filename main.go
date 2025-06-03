64f39f99206f6f067bf43538eef8992581abeeff
sat bulsara <satbulsara@Admins-MacBook-Air.local>
2025-06-03 11:54:15 +0100

    my first go lang project

---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+import "fmt"
+
+// This is a placeholder for the main function
+func main() {
+	//fmt.Print("Sat, number 2, no funfact")
+	// var student1 string
+	// student1 = "Sat"
+	// fmt.Print(student1)
+	// var attended bool
+	// attended = true
+	var name string
+	var luckyNumber int
+	var funFact string
+
+	name = "Sat"
+	luckyNumber = 2
+	funFact = "I love to play chess."
+	fmt.Println(name)
+	fmt.Println(luckyNumber)
+	fmt.Println(funFact)
+}
